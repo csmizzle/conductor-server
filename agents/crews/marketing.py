effb950f6dd7a364070b3bcb2bac59a306a4246d
csmizzle <chrissmith700@gmail.com>
2024-05-05 16:27:39 -0400

    updating marketing pipeline

--- agents/crews/marketing.py
+++ agents/crews/marketing.py
@@ -1,8 +1,6 @@
 from conductor.agents import (
-    query_builder_agent,
     apollo_email_agent,
     answer_agent,
-    create_query_task,
     apollo_email_task,
     answer_email_task
 )
@@ -11,12 +9,10 @@ from crewai import Crew
 
 market_email_crew = Crew(
     agents=[
-        query_builder_agent,
         apollo_email_agent,
         answer_agent,
     ],
     tasks=[
-        create_query_task,
         apollo_email_task,
         answer_email_task,
     ],
