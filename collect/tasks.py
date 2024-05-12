a388bc272ae5a282c60fd47757f9f724b216a907
csmizzle <chrissmith700@gmail.com>
2024-05-12 10:44:42 -0400

    fixed twisted

--- collect/tasks.py
+++ collect/tasks.py
@@ -2,12 +2,7 @@
 Celery tasks for the collect app.
 """
 from celery import shared_task
-from conductor.functions.apify_ import sync_summarize_urls
-from collect import models
-import logging
-
-
-logger = logging.getLogger(__name__)
+from collect.utils import run_url_summary_task
 
 
 @shared_task
@@ -16,28 +11,4 @@ def task_collect_summarize_urls(task_id: str, urls: list[str]) -> None:
     Task for summarizing URLs.
     """
     # get task and update status to running
-    task = models.Task.objects.get(pk=task_id)
-    logging.info(f"Task {task_id} started ...")
-    task.status = models.TaskStatus.RUNNING
-    task.save()
-    try:
-        summaries = sync_summarize_urls(urls=urls, task_id=task_id)
-        # serialize the data into URLSummary objects
-        for summary in summaries:
-            logging.info(f"Saving summary for {summary["job_id"]} ...")
-            summary_object = models.URLSummary(
-                job_id=summary["job_id"],
-                task_id=task,
-                url=summary["url"],
-                content=summary["content"],
-                summary=summary["summary"],
-            )
-            summary_object.save()
-        # update task status to completed
-        task.status = models.TaskStatus.COMPLETED
-        task.save()
-    except Exception as e:
-        logging.error(f"Error in task {task_id}: {str(e)}")
-        task.status = models.TaskStatus.FAILED
-        task.save()
-        raise e
+    run_url_summary_task(urls=urls, task_id=task_id)
---
+++ collect/utils.py
@@ -0,0 +1,36 @@
+"""
+Utility functions for the collect module.
+"""
+from conductor.functions.apify_ import sync_summarize_urls
+from collect import models
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def run_url_summary_task(urls: list[str], task_id: str) -> None:
+    task = models.Task.objects.get(pk=task_id)
+    logging.info(f"Task {task_id} started ...")
+    task.status = models.TaskStatus.RUNNING
+    task.save()
+    try:
+        summaries = sync_summarize_urls(urls=urls, task_id=task_id, stop=False)
+        # serialize the data into URLSummary objects
+        for summary in summaries:
+            logging.info(f"Saving summary for {summary["job_id"]} ...")
+            summary_object = models.URLSummary(
+                job_id=summary["job_id"],
+                task_id=task,
+                url=summary["url"],
+                content=summary["content"],
+                summary=summary["summary"],
+            )
+            summary_object.save()
+        # update task status to completed
+        task.status = models.TaskStatus.COMPLETED
+        task.save()
+    except Exception as e:
+        logging.error(f"Error in task {task_id}: {str(e)}")
+        task.status = models.TaskStatus.FAILED
+        task.save()
+        raise e
