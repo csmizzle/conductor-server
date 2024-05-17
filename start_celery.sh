c675e01ae27af6718a65b97f894f3cd146cbdba2
csmizzle <chrissmith700@gmail.com>
2024-05-17 08:56:17 -0400

    removing double admin check in shell scripts

--- start_celery.sh
+++ start_celery.sh
@@ -1,13 +1,3 @@
-echo "[!] Creating Conductor tables ..."
-
-python manage.py makemigrations && \
-python manage.py migrate
-
-if [ -z "$DJANGO_SUPERUSER_USERNAME" ] || [ -z "$DJANGO_SUPERUSER_EMAIL" ] || [ -z "$DJANGO_SUPERUSER_PASSWORD" ]
-then
-    python manage.py ensure_adminuser --noinput
-fi
-
 echo "[!] Starting Conductors Celery Server ..."
 celery -A conductor_server worker -l INFO
 
