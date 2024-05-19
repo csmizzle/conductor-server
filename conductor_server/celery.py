a8a5922d73de56b767ff587f6ca08cee11019f0d
csmizzle <chrissmith700@gmail.com>
2024-05-19 17:03:29 -0400

    adding celery integration

--- conductor_server/celery.py
+++ conductor_server/celery.py
@@ -1,9 +1,21 @@
 import os
-
 from celery import Celery
+import logfire
+from opentelemetry.instrumentation.celery import CeleryInstrumentor
+from celery.signals import worker_process_init
+
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conductor_server.settings")
+
+# set up logging and logfire
+logfire.configure()
+
+
+@worker_process_init.connect(weak=False)
+def init_celery_tracing(*args, **kwargs):
+    CeleryInstrumentor().instrument()
+
 
 app = Celery("proj")
 
