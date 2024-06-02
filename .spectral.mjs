e81fd343e3c4f2f7124c711648f66400b05df99e
chris <chris.smith@syrinxlabs.com>
2024-06-02 14:40:51 -0400

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0ODQ4NQ/spectral.js?branch_slug=main&token=819a307a-aae5-43fa-9e6f-7bc0380c21a4";
+export default { extends: ruleset };
